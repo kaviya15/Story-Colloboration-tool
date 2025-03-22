42183f57dc733913b3cbe27dcd36d3f8733f3435
p250155 <akhiljith.kumar.2025@mumail.ie>
2025-03-22 02:40:42 +0000

    Temp connection to Atlasdb

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
-const app = express();
 require("dotenv").config();
+const connectDB = require("./src/config/db");
+
+connectDB();
+
+const app = express();
+app.use(express.json());
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log(" 🚀 server started listening at port", process.env.PORT || 5000);
+  console.log(" ✅ server started listening at port", process.env.PORT || 5000);
 });
