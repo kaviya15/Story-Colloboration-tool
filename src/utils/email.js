270c58908925aace266ccb72f66abb5694683619
kaviya15 <kaviya.sivaraj1507@gmail.com>
2025-04-07 19:02:06 +0100

    email notification service

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,1 +1,21 @@
 // our emailing functions goes here
+const sgMail = require("@sendgrid/mail");
+
+module.exports.sendEmail = async (userEmail, storyTitle, userName) => {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: userEmail, // Change to your recipient
+    from: "kaviya.sivaraj1507@gmail.com", // Change to your verified sender
+    subject: "Message from Plotline",
+    text: "Hey there user" + userName,
+    html: `<strong>Wanna write some story ??? The story <em> ${storyTitle} </em> is available to edit now.  </strong>`,
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      return true;
+    })
+    .catch((error) => {
+      return false;
+    });
+};
