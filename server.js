6fb37890e74b7293f549ad7ffcdefcf8f6e3cca2
kaviya15 <kaviya.sivaraj1507@gmail.com>
2025-04-07 19:03:38 +0100

    add socket connection frontend to backend

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-
+const httpserver = require("http");
+const { Server } = require("socket.io");
 require("dotenv").config();
 const { connectDB } = require("./src/config/db");
 const routes = require("./src/routes");
-
+const { editFunction } = require("./src/sockets/socketConnection");
+const cors = require("cors");
 const app = express();
+app.use(cors());
+const server = httpserver.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", routes);
-app.listen(process.env.PORT || 5000, async () => {
+
+server.listen(process.env.PORT || 5000, async () => {
   await connectDB();
+  editFunction(io);
   console.log(" ✅ server started listening at port", process.env.PORT || 5000);
 });
