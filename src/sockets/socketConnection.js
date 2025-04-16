ddaa8442858f795bd068faca95f7ce11fe53460c
abetheresa <abetheresasp@gmail.com>
2025-04-16 11:08:50 +0100

    try-catch

--- src/sockets/socketConnection.js
+++ src/sockets/socketConnection.js
@@ -4,14 +4,22 @@ module.exports.editFunction = (io) => {
     console.log("user connected", socket.id);
     socket.on("edit", async (storyId, userId) => {
       console.log("Edit is triggered");
+      try {
       const response = await lockStoryService(storyId, { userId });
       if (response) socket.broadcast.emit("lock_story", { storyId, userId });
+      }catch(err){
+        console.error("Error locking story:", err);
+      }
     });
 
     socket.on("publish", async (storyId, userId) => {
       console.log("publish is triggered");
+      try {
       socket.broadcast.emit("unlock_story", { storyId, userId });
       /**notify all the users  */
+      }catch(err){
+        console.error("Error while broadcasting:", err);
+      }
     });
   });
 };
