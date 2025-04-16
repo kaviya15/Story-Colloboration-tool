3710eec7d49bb5f55d6d773b596b5555e44bf854
kaviya15 <kaviya.sivaraj1507@gmail.com>
2025-04-16 20:24:30 +0100

    socket conn

--- src/sockets/socketConnection.js
+++ src/sockets/socketConnection.js
@@ -1,23 +1,21 @@
-const { lockStoryService } = require("../services/storyService");
 module.exports.editFunction = (io) => {
   io.on("connection", (socket) => {
     console.log("user connected", socket.id);
     socket.on("edit", async (storyId, userId) => {
       console.log("Edit is triggered");
       try {
-      const response = await lockStoryService(storyId, { userId });
-      if (response) socket.broadcast.emit("lock_story", { storyId, userId });
-      }catch(err){
+        socket.broadcast.emit("lock_story", { storyId, userId });
+      } catch (err) {
         console.error("Error locking story:", err);
       }
     });
 
-    socket.on("publish", async (storyId, userId) => {
+    socket.on("publish", (storyId) => {
       console.log("publish is triggered");
       try {
-      socket.broadcast.emit("unlock_story", { storyId, userId });
-      /**notify all the users  */
-      }catch(err){
+        socket.broadcast.emit("unlock_story", { storyId });
+        /**notify all the users  */
+      } catch (err) {
         console.error("Error while broadcasting:", err);
       }
     });
